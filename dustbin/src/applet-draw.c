c188b5fa359330d45a3454efd5a6e935d390fb3f
fabounet <>
2008-02-21 02:21:00 +0000

    ajout weather

--- dustbin/src/applet-draw.c
+++ dustbin/src/applet-draw.c
@@ -3,7 +3,7 @@
 This file is a part of the cairo-dock project, 
 released under the terms of the GNU General Public License.
 
-Written by Fabrice Rey (for any bug report, please mail me to fabounet_03@yahoo.fr)
+Written by Fabrice Rey (for any bug report, please mail me to fabounet@users.berlios.de)
 
 **********************************************************************************/
 #include <string.h>
