7de6d1f8f0fbbb23207d4f5a239e65d81f0b833f
paradoxxx_zero <>
2008-04-08 18:15:09 +0000

    Cpusage > dummy commit

---
+++ cpusage/src/applet-rame.h
@@ -0,0 +1,11 @@
+#ifndef __APPLET_RAME__
+#define  __APPLET_RAME__
+
+#include <cairo-dock.h>
+gboolean inDebug;
+gboolean cd_rame_timer(gpointer data);
+gboolean cd_rame_getRate(void);
+void cd_rame_get_data (void);
+void cd_rame_launch_analyse(void);
+void cd_rame_formatRate(unsigned long long rate, gchar* debit);
+#endif
---
+++ cpusage/src/applet-struct.h
@@ -0,0 +1,32 @@
+
+#ifndef __CD_APPLET_STRUCT__
+#define  __CD_APPLET_STRUCT__
+
+#include <cairo-dock.h>
+
+typedef struct {
+	gchar *defaultTitle;
+/*	gchar *cDefault;
+	gchar *cUnknown;
+	gchar *cBad;
+	gchar *cOk;*/
+	gint iCheckInterval;
+	gint dCheckInterval;
+	
+	gchar *cThemePath;
+	gboolean gaugeIcon; 
+} AppletConfig;
+
+typedef struct {
+/*	cairo_surface_t *pDefault;
+	cairo_surface_t *pUnknown;	
+	cairo_surface_t *pBad;	
+	cairo_surface_t *pOk;	*/
+	guint iSidTimer;
+	gint checkTimer;
+
+	Gauge *pGauge;
+} AppletData;
+
+
+#endif
