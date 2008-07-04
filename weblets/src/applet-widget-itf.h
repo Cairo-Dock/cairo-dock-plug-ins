7a96b31bb0d8dd69b1c5ec26aead6a159dffadf1
tofe <>
2008-07-04 19:17:27 +0000

    weblets: fichiers manquants...

---
+++ weblets/src/applet-widget-itf.h
@@ -0,0 +1,35 @@
+/*
+** Login : <chris.chapuis@gmail.com>
+** Started on  Thu Apr 03 18:21:35 2008 CHAPUIS Christophe
+** $Id$
+**
+** Copyright (C) 2008 CHAPUIS Christophe
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#ifndef __CD_WEBLETS_WIDGET_ITF__
+#define  __CD_WEBLETS_WIDGET_ITF__
+
+#include "gtk/gtk.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+gboolean set_gecko_scrollbars( GtkWidget *moz, gboolean bShowScrollbars, gint scrollX, gint scrollY );
+#ifdef __cplusplus
+}
+#endif
+
+#endif
