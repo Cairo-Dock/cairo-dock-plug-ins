32868e1982c8322d2a348f856628c47db1b6836c
nochka85 <nochka85@nochka85-desktop>
2009-09-04 14:08:04 +0200

    Dbus demo_bash : auto working dir correction

--- Dbus/demos/demo_bash/demo_bash.py
+++ Dbus/demos/demo_bash/demo_bash.py
@@ -20,8 +20,10 @@ from dbus.mainloop.glib import DBusGMainLoop
 
 
 ### env variables ###
-app_folder = os.getcwd()	# name of the script folder
+app_folder = os.path.dirname(sys.argv[0])	# name of the script folder
 app_name = os.popen("TEMP=`basename " + sys.argv[0] +"` && echo $TEMP | cut -f1 -d '.' ").read().rstrip()	# name of the script without th '.py' extension AND without the './' if running as a script
+if app_folder == "":
+	app_folder = os.getcwd()
 
 
 ### let's connect to the dock.###
@@ -37,7 +39,7 @@ dock_iface = dbus.Interface(dock_object, "org.cairodock.CairoDock")
 
 
 ### let's register our applet !###
-return_from_bash = os.popen(app_folder + "/" + app_name + ".sh " + " register_the_applet").read().rstrip()
+return_from_bash = os.popen("cd " + app_folder + " && ./" + app_name + ".sh " + " register_the_applet").read().rstrip()
 try:
 	applet_object = bus.get_object("org.cairodock.CairoDock",
 			"/org/cairodock/CairoDock/" + app_name)
@@ -62,58 +64,46 @@ def init():
 	action_on_init()
 
 
-### stop ###
-def stop():
-	print "STOP"
-	bus = dbus.SessionBus()
-	remote_object = bus.get_object("org.cairodock.CairoDock", "/org/cairodock/CairoDock")
-	iface = dbus.Interface(remote_object, "org.cairodock.CairoDock")
-	iface.UnregisterModule(app_name)
-	del iface
-	del remote_object
-	del bus
-	
-
 ### callbacks ###
 def action_on_click(iState):
-	return_from_bash = os.popen(app_folder + "/" + app_name + ".sh " + " action_on_click").read().rstrip()
+	return_from_bash = os.popen("cd " + app_folder + " && ./" + app_name + ".sh " + " action_on_click").read().rstrip()
 	print return_from_bash
 	
 def action_on_middle_click():
-	return_from_bash = os.popen(app_folder + "/" + app_name + ".sh " + " action_on_middle_click").read().rstrip()
+	return_from_bash = os.popen("cd " + app_folder + " && ./" + app_name + ".sh " + " action_on_middle_click").read().rstrip()
 	print return_from_bash
 
 def action_on_scroll_icon(bUpOrDown):
 	if bUpOrDown:
-		return_from_bash = os.popen(app_folder + "/" + app_name + ".sh " + " action_on_scroll_icon 0").read().rstrip()
+		return_from_bash = os.popen("cd " + app_folder + " && ./" + app_name + ".sh " + " action_on_scroll_icon 0").read().rstrip()
 		print return_from_bash
 	else:
-		return_from_bash = os.popen(app_folder + "/" + app_name + ".sh " + " action_on_scroll_icon 1").read().rstrip()
+		return_from_bash = os.popen("cd " + app_folder + " && ./" + app_name + ".sh " + " action_on_scroll_icon 1").read().rstrip()
 		print return_from_bash
 
 def action_on_drop_data(sDataName):
-	return_from_bash = os.popen(app_folder + "/" + app_name + ".sh " + " action_on_drop_data " + sDataName).read().rstrip()
+	return_from_bash = os.popen("cd " + app_folder + " && ./" + app_name + ".sh " + " action_on_drop_data " + sDataName).read().rstrip()
 	print return_from_bash
 
 def action_on_init():
-	return_from_bash = os.popen(app_folder + "/" + app_name + ".sh " + " action_on_init").read().rstrip()
+	return_from_bash = os.popen("cd " + app_folder + " && ./" + app_name + ".sh " + " action_on_init").read().rstrip()
 	print return_from_bash
 	
 def action_on_stop():
-	return_from_bash = os.popen(app_folder + "/" + app_name + ".sh " + " action_on_stop").read().rstrip()
+	return_from_bash = os.popen("cd " + app_folder + " && ./" + app_name + ".sh " + " action_on_stop").read().rstrip()
 	print return_from_bash
 	
 def action_on_reload(bConfigHasChanged):
 	if bConfigHasChanged:
-		return_from_bash = os.popen(app_folder + "/" + app_name + ".sh " + " action_on_reload").read().rstrip()
+		return_from_bash = os.popen("cd " + app_folder + " && ./" + app_name + ".sh " + " action_on_reload").read().rstrip()
 		print return_from_bash
 
 def action_on_build_menu():
-	return_from_bash = os.popen(app_folder + "/" + app_name + ".sh " + " action_on_build_menu").read().rstrip()
+	return_from_bash = os.popen("cd " + app_folder + " && ./" + app_name + ".sh " + " action_on_build_menu").read().rstrip()
 	print return_from_bash
 	
 def action_on_menu_select(iNumEntry):
-	return_from_bash = os.popen(app_folder + "/" + app_name + ".sh " + " action_on_menu_select " + str(iNumEntry)).read().rstrip()
+	return_from_bash = os.popen("cd " + app_folder + " && ./" + app_name + ".sh " + " action_on_menu_select " + str(iNumEntry)).read().rstrip()
 	print return_from_bash
 
 
