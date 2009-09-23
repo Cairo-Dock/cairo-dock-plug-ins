d3a76c7798bed11f00907a67b8d5d17a10a9b69b
matttbe <>
2009-09-23 14:13:43 +0200

    Translations (thanks to the contributors) + fixed compilation error

--- switcher/src/applet-draw.h
+++ switcher/src/applet-draw.h
@@ -39,6 +39,8 @@ void cd_switcher_draw_main_icon (void);
 void cd_switcher_draw_desktops_bounding_box (CairoDesklet *pDesklet);
 void cd_switcher_extract_viewport_coords_from_picked_object (CairoDesklet *pDesklet, int *iCoordX, int *iCoordY);
 
+void cd_switcher_build_windows_list (GtkWidget *pMenu);
+
 
 #endif
 
