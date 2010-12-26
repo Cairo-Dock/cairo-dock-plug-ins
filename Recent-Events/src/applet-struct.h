9286701c6f352001a118f9f9d5fc572de69b9c59
Fabrice Rey <fabounet03@gmail.com>
2010-12-26 03:57:20 +0100

    added Recent Events applet (currently support recent files related to a launcher or an appli on right click)

---
+++ Recent-Events/src/applet-struct.h
@@ -0,0 +1,55 @@
+/**
+* This file is a part of the Cairo-Dock project
+*
+* Copyright : (C) see the 'copyright' file.
+* E-mail    : see the 'copyright' file.
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 3
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef __CD_APPLET_STRUCT__
+#define  __CD_APPLET_STRUCT__
+
+#include <cairo-dock.h>
+#include <zeitgeist.h>
+
+//\___________ structure containing the applet's configuration parameters.
+struct _AppletConfig {
+	gboolean bSomeBooleanValue;
+	gint iSomeIntegerValue;
+	gchar *cSomeStringValue;
+	} ;
+
+typedef void (* CDOnGetEventsFunc) (ZeitgeistResultSet *pEvents, gpointer data);
+typedef void (* CDOnDeleteEventsFunc) (gpointer data);
+typedef enum {
+	CD_EVENT_ALL,
+	CD_EVENT_DOCUMENT,
+	CD_EVENT_FOLDER,
+	CD_EVENT_IMAGE,
+	CD_EVENT_AUDIO,
+	CD_EVENT_VIDEO,
+	CD_EVENT_OTHER,
+	CD_NB_EVENT_TYPES
+	} CDEventType;
+
+//\___________ structure containing the applet's data, like surfaces, dialogs, results of calculus, etc.
+struct _AppletData {
+	ZeitgeistEvent **pEvents;
+	ZeitgeistLog *pLog;
+	ZeitgeistIndex *pIndex;
+	} ;
+
+
+#endif
