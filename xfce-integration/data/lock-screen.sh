942a4a979787d6711eb10b417de79836d58c8614
Fabrice Rey <fabounet03@gmail.com>
2011-10-02 14:47:44 +0200

    xfce integration: fixed the detection of the screensaver

--- xfce-integration/data/lock-screen.sh
+++ xfce-integration/data/lock-screen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if test -n "`ps -ef | grep gnome-screensaver`"; then
+if test -n "`ps -ef | grep gnome-screensaver | grep -v grep`"; then  ## "gnome-screensaver" is too long for pgrep
 	gnome-screensaver-command --lock
-elif test -n "`ps -ef | grep xscreensaver`"; then
+elif test -n "`ps -ef | grep xscreensaver | grep -v grep`"; then
 	xscreensaver-command -lock
 else
 	xlock
