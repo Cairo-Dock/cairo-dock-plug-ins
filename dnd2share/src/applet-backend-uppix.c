d1861b54380cfe5ba3c83f043a52e1197c21fbfb
Matthieu Baerts <matttbe@gmail.com>
2013-06-23 00:05:33 +0200

    Dnd2share: replaced uppix.net by uppix.com because uppix.net now uses JavaScript, etc. Note that we can revert this change if someone finds a way to upload on uppix.net but it doesn't work with:   $ curl -L uppix.net/upload -H Expect: -F files=@FILE_HERE -F token=UR37GjfK9as1qsH76FUYkUrX

--- dnd2share/src/applet-backend-uppix.c
+++ dnd2share/src/applet-backend-uppix.c
@@ -27,93 +27,38 @@
 #include "applet-dnd2share.h"
 #include "applet-backend-uppix.h"
 
-#define NB_URLS 5
-static const gchar *s_UrlLabels[NB_URLS] = {"DirectLink", "DisplayImage", "BBCode150px", "BBCode600px", "BBCodeFullPic"};
+#define NB_URLS 3
+static const gchar *s_UrlLabels[NB_URLS] = {"DirectLink", "Thumbnail", "BBCode"};
 
 
 static void upload (const gchar *cFilePath, gchar *cDropboxDir, gboolean bAnonymous, gint iLimitRate, gchar **cResultUrls)
 {
-	// On cree un fichier de log temporaire.
-	gchar *cLogFile = g_strdup ("/tmp/dnd2share-log.XXXXXX");
-	int fds = mkstemp (cLogFile);
-	if (fds == -1)
-	{
-		g_free (cLogFile);
-		return ;
-	}
-	close(fds);
-	
 	// On lance la commande d'upload.
-	gchar *cCommand = g_strdup_printf ("curl -L --connect-timeout 5 --retry 2 --limit-rate %dk uppix.net -H Expect: -F myimage=@\"%s\" -F submit=Upload -F formup=1 -o \"%s\"", iLimitRate, cFilePath, cLogFile);
+	gchar *cCommand = g_strdup_printf ("curl -L --connect-timeout 5 --retry 2 --limit-rate %dk uppix.com/upload -H Expect: -F u_file=@\"%s\" -F u_submit=Upload -F u_agb=yes", iLimitRate, cFilePath);
 	cd_debug ("%s", cCommand);
-	int r = system (cCommand);
-	if (r < 0)
-		cd_warning ("Not able to launch this command: %s", cCommand);
+	gchar *cResult = cairo_dock_launch_command_sync (cCommand);
 	g_free (cCommand);
-	
-	
-	// On récupère toutes les infos dans le fichier de log :
-	/// TODO : se passer des g_spawn_command_line_sync !
-	//D'abord l'url de DisplayImage
-	gchar *cDisplayImage = NULL;
-	gchar *cCommandDisplayImage = g_strdup_printf ("grep -oEm 1 '\\[url\\=([^]]*)' \"%s\"", cLogFile);
-	g_spawn_command_line_sync (cCommandDisplayImage, &cDisplayImage,  NULL, NULL, NULL);
-	g_free (cCommandDisplayImage);
-	
-	if (cDisplayImage == NULL || *cDisplayImage == '\0')
-	{
-		g_remove (cLogFile);
-		g_free (cLogFile);
-		return ;
-	}
-	
-	gchar *str = g_strstr_len (cDisplayImage, -1, "http://"); // On retire tout ce qui se trouve avant http://
-	if (str != NULL && str != cDisplayImage)
-	{
-		gchar *tmp = cDisplayImage;
-		cDisplayImage = g_strdup (str);
-		g_free (tmp);
-	}
-	cDisplayImage[strlen(cDisplayImage) - 1] = '\0';  // on retire le \n à la fin
-	cd_debug ("dnd2share : Display Image = %s", cDisplayImage);
-	
-	// Puis l'url de DirectLink
-	gchar *cDirectLink = NULL;
-	gchar *cCommandDirectLink = g_strdup_printf ("grep -oEm 1 '\\[img\\]([^[]*)' \"%s\"", cLogFile);
-	g_spawn_command_line_sync (cCommandDirectLink, &cDirectLink,  NULL, NULL, NULL);
-	g_free (cCommandDirectLink);
-	
-	str = g_strstr_len (cDirectLink, -1, "http://"); // On retire tout ce qui se trouve avant http://
-	if (str != NULL && str != cDirectLink)
-	{
-		gchar *tmp = cDirectLink;
-		cDirectLink = g_strdup (str);
-		g_free (tmp);
-	}
-	cDirectLink[strlen(cDirectLink) - 1] = '\0';  // on retire le \n à la fin
-	cd_debug ("dnd2share : Direct Link = %s", cDirectLink);
 
-	// Puis on créé les autres URLs à la mano ;-) :
-	gchar *cBBCodeFullPic = g_strdup_printf ("[img]%s[/img]", cDirectLink);
-	cd_debug ("dnd2share : BBCODE_Full = '%s'", cBBCodeFullPic);
-	
-	gchar *cDirectLinkWithoutExt = g_strdup (cDisplayImage);
-	cDirectLinkWithoutExt[strlen(cDirectLinkWithoutExt) - 5] = '\0';  // on retire le .html à la fin
-	gchar *cBBCode150px = g_strdup_printf ("[url=%s][img]%st.jpg[/img][/url]", cDirectLink, cDirectLinkWithoutExt);
-	gchar *cBBCode600px = g_strdup_printf ("[url=%s][img]%stt.jpg[/img][/url]", cDirectLink, cDirectLinkWithoutExt);
-	g_free (cDirectLinkWithoutExt);
-	cd_debug ("dnd2share : BBCODE_150px = '%s'", cBBCode150px);
-	cd_debug ("dnd2share : BBCODE_600px = '%s'", cBBCode600px);
-	
-	g_remove (cLogFile);
-	g_free (cLogFile);
-	
+	if (! cResult)
+		return;
+
+	gchar *cDirectLinkStart = strstr (cResult, "http://uppix.com/"); // find the direct link
+	if (cDirectLinkStart)
+	{
+		gchar *cDirectLinkEnd = strstr (cDirectLinkStart, "&quot;");
+		if (cDirectLinkEnd)
+			*cDirectLinkEnd = '\0';
+	}
+	gchar *cDirectLink = g_strdup (cDirectLinkStart); // http://uppix.com/f-(...)
+	g_free (cResult);
+
+	gchar *cThumbnail = g_strdup (cDirectLink); // the same url but with 't' instead of 'f' => http://uppix.com/t-(...)
+	cThumbnail[17] = 't';
+
 	// Enfin on remplit la memoire partagee avec nos URLs.
 	cResultUrls[0] = cDirectLink;
-	cResultUrls[1] = cDisplayImage;
-	cResultUrls[2] = cBBCode150px;
-	cResultUrls[3] = cBBCode600px;
-	cResultUrls[4] = cBBCodeFullPic;
+	cResultUrls[1] = cThumbnail;
+	cResultUrls[2] = g_strdup_printf ("[url=%s][img]%s[/img][/url]", cDirectLink, cThumbnail);
 }
 
 
