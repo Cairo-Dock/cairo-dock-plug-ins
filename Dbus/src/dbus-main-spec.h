8ba5a5abb59376c145b545704f506c28cf193939
Kondor Dániel <kondor.dani@gmail.com>
2025-12-27 23:19:13 +0100

    DBus: minimial changes to make things compile

--- Dbus/src/dbus-main-spec.h
+++ Dbus/src/dbus-main-spec.h
@@ -594,32 +594,3 @@ G_END_DECLS
 
 #endif /* __dbus_glib_marshal_cd_dbus_main_MARSHAL_H__ */
 
-#include <dbus/dbus-glib.h>
-static const DBusGMethodInfo dbus_glib_cd_dbus_main_methods[] = {
-  { (GCallback) cd_dbus_main_reboot, dbus_glib_marshal_cd_dbus_main_BOOLEAN__POINTER, 0 },
-  { (GCallback) cd_dbus_main_quit, dbus_glib_marshal_cd_dbus_main_BOOLEAN__POINTER, 34 },
-  { (GCallback) cd_dbus_main_show_dock, dbus_glib_marshal_cd_dbus_main_BOOLEAN__INT_POINTER, 66 },
-  { (GCallback) cd_dbus_main_show_desklet, dbus_glib_marshal_cd_dbus_main_BOOLEAN__BOOLEAN_POINTER, 118 },
-  { (GCallback) cd_dbus_main_add, dbus_glib_marshal_cd_dbus_main_BOOLEAN__BOXED_POINTER_POINTER, 173 },
-  { (GCallback) cd_dbus_main_reload, dbus_glib_marshal_cd_dbus_main_BOOLEAN__STRING_POINTER, 244 },
-  { (GCallback) cd_dbus_main_remove, dbus_glib_marshal_cd_dbus_main_BOOLEAN__STRING_POINTER, 289 },
-  { (GCallback) cd_dbus_main_get_properties, dbus_glib_marshal_cd_dbus_main_BOOLEAN__STRING_POINTER_POINTER, 334 },
-  { (GCallback) cd_dbus_main_set_quick_info, dbus_glib_marshal_cd_dbus_main_BOOLEAN__STRING_STRING_POINTER, 411 },
-  { (GCallback) cd_dbus_main_set_label, dbus_glib_marshal_cd_dbus_main_BOOLEAN__STRING_STRING_POINTER, 481 },
-  { (GCallback) cd_dbus_main_set_icon, dbus_glib_marshal_cd_dbus_main_BOOLEAN__STRING_STRING_POINTER, 543 },
-  { (GCallback) cd_dbus_main_set_emblem, dbus_glib_marshal_cd_dbus_main_BOOLEAN__STRING_INT_STRING_POINTER, 604 },
-  { (GCallback) cd_dbus_main_animate, dbus_glib_marshal_cd_dbus_main_BOOLEAN__STRING_INT_STRING_POINTER, 681 },
-  { (GCallback) cd_dbus_main_demands_attention, dbus_glib_marshal_cd_dbus_main_BOOLEAN__BOOLEAN_STRING_STRING_POINTER, 760 },
-  { (GCallback) cd_dbus_main_show_dialog, dbus_glib_marshal_cd_dbus_main_BOOLEAN__STRING_INT_STRING_POINTER, 845 },
-  { (GCallback) cd_dbus_main_set_menu, dbus_glib_marshal_cd_dbus_main_BOOLEAN__STRING_STRING_STRING_POINTER, 924 },
-  { (GCallback) cd_dbus_main_set_progress, dbus_glib_marshal_cd_dbus_main_BOOLEAN__DOUBLE_STRING_POINTER, 1001 },
-};
-
-const DBusGObjectInfo dbus_glib_cd_dbus_main_object_info = {  1,
-  dbus_glib_cd_dbus_main_methods,
-  17,
-"org.cairodock.CairoDock\0Reboot\0S\0\0org.cairodock.CairoDock\0Quit\0S\0\0org.cairodock.CairoDock\0ShowDock\0S\0iVisibility\0I\0i\0\0org.cairodock.CairoDock\0ShowDesklet\0S\0widgetLayer\0I\0b\0\0org.cairodock.CairoDock\0Add\0S\0pProperties\0I\0a{sv}\0cConfigFile\0O\0F\0N\0s\0\0org.cairodock.CairoDock\0Reload\0S\0cQuery\0I\0s\0\0org.cairodock.CairoDock\0Remove\0S\0cQuery\0I\0s\0\0org.cairodock.CairoDock\0GetProperties\0S\0cQuery\0I\0s\0pProperties\0O\0F\0N\0aa{sv}\0\0org.cairodock.CairoDock\0SetQuickInfo\0S\0cQuickInfo\0I\0s\0cIconQuery\0I\0s\0\0org.cairodock.CairoDock\0SetLabel\0S\0cLabel\0I\0s\0cIconQuery\0I\0s\0\0org.cairodock.CairoDock\0SetIcon\0S\0cImage\0I\0s\0cIconQuery\0I\0s\0\0org.cairodock.CairoDock\0SetEmblem\0S\0cImage\0I\0s\0iPosition\0I\0i\0cIconQuery\0I\0s\0\0org.cairodock.CairoDock\0Animate\0S\0cAnimation\0I\0s\0iNbRounds\0I\0i\0cIconQuery\0I\0s\0\0org.cairodock.CairoDock\0DemandsAttention\0S\0bStart\0I\0b\0cAnimation\0I\0s\0cIconQuery\0I\0s\0\0org.cairodock.CairoDock\0ShowDialog\0S\0message\0I\0s\0iDuration\0I\0i\0cIconQuery\0I\0s\0\0org.cairodock.CairoDock\0SetMenu\0S\0cBusName\0I\0s\0cMenuPath\0I\0s\0cIconQuery\0I\0s\0\0org.cairodock.CairoDock\0SetProgress\0S\0fPercent\0I\0d\0cIconQuery\0I\0s\0\0\0",
-"\0",
-"\0"
-};
-
